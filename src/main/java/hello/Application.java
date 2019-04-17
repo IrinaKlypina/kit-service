de3e7fad47c89959a744f97366fe5aa0687b775a
Irina <irulya95@gmail.com>
2019-04-17 17:34:18 +0800

    create a simple spring server

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,12 @@
 package hello;
 
-import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class HelloWorld {
+@SpringBootApplication
+public class Application {
+
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        SpringApplication.run(Application.class, args);
     }
 }
